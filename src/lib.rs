f433f3f0f93d01ed6be4d48bcfd48d16ae470e51
spacemeowx2 <spacemeowx2@gmail.com>
2021-03-19 21:39:24 +0800

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::{
+    path::Path,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+pub use notify;
+
+use futures_channel::mpsc;
+use futures_core::stream::Stream;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+
+type StreamItem = notify::Result<notify::Event>;
+pub struct NotifyStream(mpsc::UnboundedReceiver<StreamItem>, RecommendedWatcher);
+
+impl Stream for NotifyStream {
+    type Item = StreamItem;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        Pin::new(&mut self.0).poll_next(cx)
+    }
+}
+
+pub fn notify_stream<P: AsRef<Path>>(
+    path: P,
+    recursive_mode: RecursiveMode,
+) -> notify::Result<impl Stream<Item = StreamItem>> {
+    let (tx, rx) = mpsc::unbounded();
+
+    let mut watcher: RecommendedWatcher =
+        Watcher::new_immediate(move |i: notify::Result<notify::Event>| {
+            // ignore error
+            let _ = tx.unbounded_send(i).ok();
+        })?;
+
+    watcher.watch(path, recursive_mode)?;
+
+    Ok(NotifyStream(rx, watcher))
+}
